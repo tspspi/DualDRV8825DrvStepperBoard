9e766f7409759b36c50c746d73e021f5a9191591
tspspi <11219971+tspspi@users.noreply.github.com>
2019-05-11 22:50:00 +0200

    Updated test program to try a more complicated pattern of accelerated movements

--- controller/tests/001_accelerations.c
+++ controller/tests/001_accelerations.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
 #include "../dualstep.h"
 
 int main(int argc, char* argv[]) {
@@ -21,6 +23,50 @@ int main(int argc, char* argv[]) {
 		i2cClose(bus2c); bus2c = NULL;
 		return -1;
 	}
+	printf("Stepper opened\n");
+
+/*	printf("Querying acceleration and deceleration: ");
+	union {
+		double accel;
+		uint64_t raw;
+	} a;
+	union {
+		double decel;
+		uint64_t raw;
+	} d;
+	step->obj.vtbl->getAccelerationDeceleration(step, 0, &a.accel, &d.decel);
+	printf("%lf and %lf (raw %08llx %08llx)\n", a.accel, d.decel, a.raw, d.raw);
+*/
+
+	printf("Running accelerated movement with default settings\n");
+	step->obj.vtbl->queueMoveAngularDistance(step, 1, -62.83185307179586476925286766559/5.0);
+	step->obj.vtbl->queueMoveAngularDistance(step, 0, 62.83185307179586476925286766559/5.0);
+	sleep(3);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, 62.83185307179586476925286766559/5.0);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, -62.83185307179586476925286766559/5.0);
+	sleep(10);
+	step->obj.vtbl->queueMoveAngularDistance(step, 1, -62.83185307179586476925286766559);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, 62.83185307179586476925286766559);
+        sleep(3);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, 62.83185307179586476925286766559);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, -62.83185307179586476925286766559);
+        sleep(3);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, -62.83185307179586476925286766559/5.0);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, 62.83185307179586476925286766559/5.0);
+        sleep(2);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, 62.83185307179586476925286766559/5.0);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, -62.83185307179586476925286766559/5.0);
+        sleep(2);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, -62.83185307179586476925286766559);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, 62.83185307179586476925286766559);
+        step->obj.vtbl->queueMoveAngularDistance(step, 1, 62.83185307179586476925286766559);
+        step->obj.vtbl->queueMoveAngularDistance(step, 0, -62.83185307179586476925286766559);
+	sleep(1);
+
+	printf("Releasing\n");
+	step->obj.vtbl->release(step);
+
+
 	i2cClose(bus2c); bus2c = NULL;
 	printf("Done\n");
 	return 0;
